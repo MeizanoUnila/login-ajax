1b0861d2f7d6eccd4a7adcd91667b9493ce22d9a
Meizano A.M. Djausal <meizanoam@gmail.com>
2016-06-13 05:15:17 +0700

    Penambahan README.MD dan crudaku.sql

---
+++ db/crudakun.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 13, 2016 at 12:12 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `crudakun`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun`
+--
+
+CREATE TABLE IF NOT EXISTS `akun` (
+  `ID` int(8) NOT NULL,
+  `login` varchar(16) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`ID`, `login`, `password`) VALUES
+(19, 'tes', '28b662d883b6d76fd96e4ddc5e9ba780');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikel`
+--
+
+CREATE TABLE IF NOT EXISTS `artikel` (
+  `ID` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `isi` text,
+  `kategori` varchar(50) DEFAULT NULL,
+  `datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `ID_User` int(11) NOT NULL COMMENT 'ambil dari table user'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `artikel`
+--
+
+INSERT INTO `artikel` (`ID`, `judul`, `isi`, `kategori`, `datetime`, `ID_User`) VALUES
+(1, 'Artikel Pertama', 'Isi artikel Pertama', NULL, '0000-00-00 00:00:00', 17),
+(2, 'Artikel kedua', NULL, NULL, '2015-05-29 08:24:51', 18),
+(3, 'Artikel kedua', NULL, NULL, '0000-00-00 00:00:00', 18);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `ID` int(11) NOT NULL,
+  `kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`ID`), ADD KEY `kategori` (`kategori`), ADD KEY `judul` (`judul`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`ID`), ADD KEY `kategori` (`kategori`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `ID` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `artikel`
+--
+ALTER TABLE `artikel`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `artikel`
+--
+ALTER TABLE `artikel`
+ADD CONSTRAINT `artikel_ibfk_1` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`kategori`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
