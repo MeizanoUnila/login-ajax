de6327c1584c8ee6bf195783d49eed25ccb07398
Meizano A.M. Djausal <meizanoam@gmail.com>
2016-12-16 04:08:19 +0700

    Mengganti default request menjadi POST dan mengedit lisensi pada READ.ME menjadi GNU GPLv3.

--- js/login.js
+++ js/login.js
@@ -25,24 +25,24 @@ $(document).ready(function () {
 
         if (username && password) { // Hanya dijalankan jika nilai tidak kosong
             $.ajax({
-                type: "GET",
-                url: "dispatcher/ws-login-GET.php", // URL dari web service JSON, dalam hal ini dilayani skrip PHP
                 dataType: "json",
+                type: "POST", //type bisa GET atau POST
 
+                // jika pakai GET, pakai:
+                // url: "dispatcher/ws-login-GET.php", // URL dari web service JSON, dalam hal ini dilayani skrip PHP
+//                data: "login=" + username + "&passwd=" + password,
                 // Mengirim login dengan nilai username dan passwd dengan nilai password sebagai parameter ke skrip PHP
-                // jika pakai POST, ganti dengan url: "dispatcher/ws-login-POST.php"
-                // data: {
-                //     login: username,
-                //     passwd: password
-                // },
-                // jika pakai GET, ganti dengan url: "dispatcher/ws-login-GET.php"
-                data: "login=" + username + "&passwd=" + password,
+
+                // jika pakai POST, pakai:
+                url: "dispatcher/ws-login-POST.php", // URL dari web service JSON, dalam hal ini dilayani skrip PHP
+                data: {login: username, passwd: password},
+                // Mengirim login dengan nilai username dan passwd dengan nilai password sebagai parameter ke skrip PHP
 
                 // Jika web service tidak merespon/gagal
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
                     $('div#hasilLogin').text("responseText: " + XMLHttpRequest.responseText +
-                        ", textStatus: " + textStatus +
-                        ", errorThrown: " + errorThrown);
+                                             ", textStatus: " + textStatus +
+                                             ", errorThrown: " + errorThrown);
                     $('div#hasilLogin').removeClass();
                     $('div#hasilLogin').addClass("alert alert-warning");
                 }, // error
@@ -50,27 +50,27 @@ $(document).ready(function () {
                 // Jika web service merespon
                 // data yang mengandung nilai JSON akan dikembalikan oleh skrip PHP
                 success: function (data) {
-                        if (data.gagal) { // key gagal dikembalikan
-                            $('div#hasilLogin').removeAttr("style");
-                            $('div#hasilLogin').removeClass();
-                            $('div#hasilLogin').text("data.gagal: " + data.gagal);
-                            $('div#hasilLogin').addClass("alert alert-warning");
-                        } else { // login berhasil
-                            $('div#loginPanel').hide(); // menyembunyikan login panel
-                            $('div#hasilLogin').removeAttr("style");
-                            $('div#hasilLogin').removeClass();
-                            $('div#hasilLogin').text("data.berhasil: " + data.berhasil +
-                                ", data.userid: " + data.userid); // key berhasil dan userid dikembalikan
-                            $('div#hasilLogin').addClass("alert alert-success");
-                            // if ($rememberMe.is(":checked")) {
-                            // Agar user tidak perlu login berikutnya, disini buat cookie
-                            // }
-                            setTimeout(function () {
-                                redirect('dashboard.html');
-                            }, 1000); // mengarahkan ke dashboard setelah 1 detik
-                            // window.location.reload(); // Jika SPA, gunakan ini
-                        }
-                    } // success
+                    if (data.gagal) { // key gagal dikembalikan
+                        $('div#hasilLogin').removeAttr("style");
+                        $('div#hasilLogin').removeClass();
+                        $('div#hasilLogin').text("data.gagal: " + data.gagal);
+                        $('div#hasilLogin').addClass("alert alert-warning");
+                    } else { // login berhasil
+                        $('div#loginPanel').hide(); // menyembunyikan login panel
+                        $('div#hasilLogin').removeAttr("style");
+                        $('div#hasilLogin').removeClass();
+                        $('div#hasilLogin').text("data.berhasil: " + data.berhasil +
+                                                 ", data.userid: " + data.userid); // key berhasil dan userid dikembalikan
+                        $('div#hasilLogin').addClass("alert alert-success");
+                        // if ($rememberMe.is(":checked")) {
+                        // Agar user tidak perlu login berikutnya, disini buat cookie
+                        // }
+                        setTimeout(function () {
+                            redirect('dashboard.html');
+                        }, 1000); // mengarahkan ke dashboard setelah 1 detik
+                        // window.location.reload(); // Jika SPA, gunakan ini
+                    }
+                } // success
             }); // ajax
         } // if
         else {
